de89e00898f5bb106ef6712082cc4b45904971f5
Daniel Cooper <62018439+dancooper37@users.noreply.github.com>
2021-06-13 19:01:03 +0100

    Solved cons, added consensus function to toolkit

---
+++ rosalind/stronghold/cons.py
@@ -0,0 +1,34 @@
+from toolkit.utilities import unpackFASTA
+from toolkit.dna import consensusNuc
+
+dnaMatrix = []
+
+dataDict = unpackFASTA("../compute_data/rosalind_cons.txt")
+
+for value in dataDict.values():
+    dnaMatrix.append(value)  # Unpacks the sequences from fasta into a list of strings
+
+dnaVector = []  # Stores the nth base from each sequence, cleared after use
+consensus = []  # List of most frequent base in each position over all sequences
+
+for base in range(len(dnaMatrix[0])):
+    for seq in range(len(dnaMatrix)):
+        dnaVector.append(dnaMatrix[seq][base])
+        if seq == len(dnaMatrix) - 1:  # Once the vector has been fully generated for each of the sequences
+            consensus.append(consensusNuc(dnaVector))  # Find the most common base from the vector
+            dnaVector.clear()  # Empty the list
+
+print("".join(consensus))
+
+
+
+
+
+
+
+
+
+
+
+
+
--- rosalind/stronghold/iprb.py
+++ rosalind/stronghold/iprb.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 k = 2  # Homozygous dominant
 m = 2  # Heterozygous
 n = 2  # Homozygous recessive
